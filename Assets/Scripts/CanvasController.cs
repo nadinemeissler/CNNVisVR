763c9fc82279c762b8c2c0c4a11e5d8f3e1181e3
nadinemeissler <15940719+nadinemeissler@users.noreply.github.com>
2019-04-18 15:10:53 +0200

    Added example images, added script CanvasController, changed 3D model

---
+++ Assets/Scripts/CanvasController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * Controlls the UI-Elements   
+ */
+
+public class CanvasController : MonoBehaviour
+{
+    public Canvas canvasMiddle;
+
+    Sprite[,] filterSprites;
+
+    private void Awake()
+    {
+        filterSprites = new Sprite[2,64];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
